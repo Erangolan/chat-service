391a4d860e1e94b201f61580bddc20c4ca675314
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-02-07 16:21:54 +0200

    added some logic to socket middleware

--- src/api/middleware/initSocket.js
+++ src/api/middleware/initSocket.js
@@ -2,6 +2,8 @@ const http = require('http')
 const socketIo = require('socket.io')
 
 const messages = []
+const connected = []
+const disconnected = []
 
 const initSocket = (app, port) => {
   const server = http.createServer(app)
@@ -17,6 +19,19 @@ const initSocket = (app, port) => {
 
   io.on('connection', (socket) => {
     console.log('new client connected: ', socket.id)
+
+    socket.on('disconnect', () => {
+      const connectedName = connected.find(({ socketId }) => socketId === socket.id)
+
+      disconnected.push({
+        socketId: socket.id,
+        name: connectedName.name || socket.handshake.query.name,
+      })
+      const userLeft = connected.find(({ socketId }) => socketId === socket.id)
+
+      console.log(`${userLeft.name} left the chat`)
+      io.emit('left', `${userLeft.name} left the chat`)
+    })
   })
 
   io.on('error', (err) => {
@@ -24,6 +39,8 @@ const initSocket = (app, port) => {
   })
   server.listen(port, () => console.log(`Listening on port ${port}`))
   io.messages = messages
+  io.connected = connected
+  io.disconnected = disconnected
   return io
 }
 
